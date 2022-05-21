baf1b538335c12408c028b20173fdc4ad094b5de
xiphiasnonus <foster.pianist@gmail.com>
2022-05-21 15:03:53 -0400

    check wad type

--- src/WadFile.ts
+++ src/WadFile.ts
@@ -1,11 +1,25 @@
 export class WadFile {
+    // type
     // data
 
     constructor(file: File) {
+        console.log(file);
         let fileReader = new FileReader();
+
         fileReader.readAsArrayBuffer(file);
-        fileReader.onloadend = function(e) {
-            
+
+        fileReader.onload = function(e) {
+            let arrayBuffer = e.target.result;
+            let wadTypeSlice = arrayBuffer?.slice(0, 4);
+            let charArray = new Uint8Array(wadTypeSlice);
+            let idString = String.fromCharCode(...charArray);
+            console.log(idString);
+            //let dataView = new DataView(arrayBuffer);
+            //console.log(dataView.getUint8(0));
+        }
+
+        fileReader.onerror = function(e) {
+            console.log('WadFile: read error');
         }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { WadFile } from "./WadFile";
+
+console.log(true);
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 const inputEl = document.querySelector('input') as HTMLInputElement;
 
+
 inputEl.addEventListener('change', (e) => {
-  console.log('inputEl');
+    const wadFile = new WadFile(e.target.files[0]);
 });
 
 console.dir(inputEl);
