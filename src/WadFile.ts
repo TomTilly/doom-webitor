49381dce3f3d12ef11c2746c10cde14c0ddb43a7
Tom Tillistrand <tomtillistrand@gmail.com>
2022-06-04 14:10:37 -0400

    Add ESLint + Prettier formatting

--- src/WadFile.ts
+++ src/WadFile.ts
@@ -1,62 +1,61 @@
-
 enum WadType {
-    iwad = "IWAD",
-    pwad = "PWAD"
+   iwad = 'IWAD',
+   pwad = 'PWAD',
 }
 
 export class DirectoryEntry {
-    filePosition: number;
-    size: number; // if bytes
-    name: string;
+   filePosition: number;
+   size: number; // if bytes
+   name: string;
 }
 
 export class WadFile {
-    type: WadType;
-    buffer: ArrayBuffer;
-    numLumps: number;
-    directory: DirectoryEntry[];
+   type: WadType;
+   buffer: ArrayBuffer;
+   numLumps: number;
+   directory: DirectoryEntry[];
 
-    constructor(file: File) {
-        const fileReader = new FileReader();
+   constructor(file: File) {
+      const fileReader = new FileReader();
 
-        fileReader.readAsArrayBuffer(file);
-        fileReader.onload = (e) => {
-            const check = e.target!.result as ArrayBuffer | null;
-            if ( check === null ) {
-                throw new Error('Error: could not get array buffer from file');
-            }
-            this.buffer = check;
-            
-            const header = new DataView(this.buffer, 0, 12);
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = (e) => {
+         const check = e.target!.result as ArrayBuffer | null;
+         if (check === null) {
+            throw new Error('Error: could not get array buffer from file');
+         }
+         this.buffer = check;
 
-            // get Header ID String
-            const idString = this.getString(header, 0, 4);
+         const header = new DataView(this.buffer, 0, 12);
 
-            if ( !(idString == WadType.iwad || idString == WadType.pwad) ) {
-                throw new Error('Error: not a valid WAD file');
-            }
+         // get Header ID String
+         const idString = this.getString(header, 0, 4);
 
-            if ( idString === WadType.iwad ) {
-                this.type = WadType.iwad;
-            } else if ( idString === WadType.pwad ) {
-                this.type = WadType.pwad;
-            }
+         if (!(idString == WadType.iwad || idString == WadType.pwad)) {
+            throw new Error('Error: not a valid WAD file');
+         }
 
-            // get number of lumps
+         if (idString === WadType.iwad) {
+            this.type = WadType.iwad;
+         } else if (idString === WadType.pwad) {
+            this.type = WadType.pwad;
+         }
 
-            this.numLumps = header.getUint32(4, true);
-            
-            // get directory offset
+         // get number of lumps
 
-            let directoryOffset = header.getUint32(8, true);
-        }
-        fileReader.onerror = function(e) {
-            throw new Error('Error: WAD file read error');
-        }
-    }
+         this.numLumps = header.getUint32(4, true);
 
-    getString(dataView: DataView, offset: number, length: number): string {
+         // get directory offset
+
+         const directoryOffset = header.getUint32(8, true);
+      };
+      fileReader.onerror = function (e) {
+         throw new Error('Error: WAD file read error');
+      };
+   }
+
+   getString(dataView: DataView, offset: number, length: number): string {
       const charArray = new Uint8Array(dataView.buffer, offset, length);
       return String.fromCharCode(...charArray);
-    }
+   }
 }
