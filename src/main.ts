12c5177405d87e0385ad77977faf92921e6b1803
Tom Tillistrand <tomtillistrand@gmail.com>
2022-06-05 22:01:56 -0400

    Correct typing of testIWAD import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { WadFile } from './WadFile';
 import testIWAD from '../wads/DOOM.WAD';
 
+if (typeof testIWAD !== 'string') {
+   throw new Error('testIWAD should be a string.');
+}
+
 const response = await fetch(testIWAD);
 const arrayBuffer = await response.arrayBuffer();
 
